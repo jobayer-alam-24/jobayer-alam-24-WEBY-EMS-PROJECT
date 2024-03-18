b692cef09ce65725975997a1b29dedb2ed7f2db0
jobayer-alam-24 <sheikhnayamul123@gmail.com>
2024-03-17 19:10:33 -0700

    WEBY EMS PROJECT

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("name").value = "Ashikq E Ahmed";
+document.getElementById("roll").value = "123321"
+document.getElementById("blood-g").value = "O+ (Positive)";
+document.getElementById("parents-name").value = "Shohidur alam";
+document.getElementById("blood-g").value = "O+ (Positive)";
+document.getElementById("fb").value = "Ashiq boss";
+document.getElementById("p-addr").value = "Ashiq boss";
+document.getElementById("parents-num").value = "019xxxxxxxx";
+document.getElementById("std-num").value = "xxxxxxxxx019";
+document.getElementById("batch-name").value = "Ligeion of Doom";
+document.getElementById("l-b-exam").value = "1971";
+document.getElementById("school-name").value = "AIUB primary School";
+document.getElementById("group").value = "Science";
+document.getElementById("previous-b-exam").value = "1966";
